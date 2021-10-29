8690365122cd95f06eadc46a4a8a6fec05a2db1e
Ty Jones-Magee <tyree.jones.magee@gmail.com>
2021-10-28 22:46:32 -0400

    Update halloween_game.java

--- halloween_game.java
+++ halloween_game.java
@@ -1,7 +1,8 @@
-import java.util.Scanner;
+import java.util.*;
 public class halloween_game{
   public static void main (String []args){
     Scanner k = new Scanner(System.in);
+    Random rand = new Random();
     System.out.println("Enter a game mode:\n"+
                        "1 for Trivia\n"+
                        "2 for Guessing");
@@ -12,6 +13,13 @@ public class halloween_game{
         
         System.out.println("You have chosen Trivia");
         System.out.println("\n\n");
+        
+        int qu = rand.nextInt(11); // random number generator for the question
+        
+        if (
+         System.out.println("
+        
+        
         
         
        break;
