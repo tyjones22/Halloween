7692d9b1fe2945610c483b7d6309e503f7f22195
Ty Jones-Magee <tyree.jones.magee@gmail.com>
2021-10-28 20:31:23 -0400

    Update halloween_guesser.java

--- halloween_guesser.java
+++ halloween_guesser.java
@@ -2,7 +2,27 @@ Import java.util.Scanner;
 public class halloween_guesser{
   public static void main (String []args){
     Scanner k = new Scanner(System.in);
+    System.out.println("Enter a game mode:\n"+
+                       "1 for Trivia\n"+
+                       "2 for Guessing");
+    int mode= k.nextInt();
     
-    System
+    switch (mode){
+      case 1:
+        
+        System.out.println("You have chosen Trivia");
+        
+       break;
+        
+      case 2:
+        
+        System.out.println("You have chosen Guessing");
+        
+       break;
+       
+      default:
+        System.out.println("Please pick an eligble option :) ");  
+    }
+    
   }
 }
