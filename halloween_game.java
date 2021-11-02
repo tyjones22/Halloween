1bebfcfef9284ac3e1b0e29b12f657f43d83cd4f
Ty Jones-Magee <tyree.jones.magee@gmail.com>
2021-11-02 17:35:17 -0400

    Update halloween_game.java

--- halloween_game.java
+++ halloween_game.java
@@ -7,6 +7,7 @@ public class halloween_game{
                        "1 for Trivia\n"+
                        "2 for Guessing");
     int mode= k.nextInt();
+    k.nextLine();
     
     switch (mode){
       case 1:
@@ -14,13 +15,17 @@ public class halloween_game{
         System.out.println("You have chosen Trivia");
         System.out.println("\n\n");
         
-       int qu = rand.nextInt(11); // random number generator for the question
+       int count, count2;
+       boolean used;
+     for ( count=1; count <=10; count++){
+       int qu = rand.nextInt(12); // random number generator for the question
         
-              //int qu =0;
-        if (qu == 0){
+           // int qu =4;
+        if (qu == 0&&!used){
+          used= true;
           System.out.println("Only one holiday surpasses Halloween in annual consumer sales. What is it?");
             String rightAnswer= "Christmas";
-            k.nextLine();
+            
             String answer = k.nextLine();
             
 
@@ -31,13 +36,13 @@ public class halloween_game{
                    System.out.println("Only one holiday surpasses Halloween in annual consumer sales. What is it?");
                    answer = k.nextLine();
             }
-            System.out.print("\nCorrect!");
+            System.out.print("\nCorrect!\n\n");
           }
         
         else if (qu == 1){
           System.out.println("What was candy corn originally called?");
            String rightAnswer= "Chicken feed";
-           k.nextLine();
+           
             String answer = k.nextLine();
             
 
@@ -48,13 +53,13 @@ public class halloween_game{
                    System.out.println("What was candy corn originally called?");
                    answer = k.nextLine();
             }
-            System.out.print("\nCorrect!");
+            System.out.print("\nCorrect!\n\n");
           }
         
         else if (qu==2){
           System.out.println("What famous magician died on Halloween?");
           String rightAnswer= "Harry Houdini";
-          k.nextLine();
+         
            String answer = k.nextLine();
            
 
@@ -65,14 +70,14 @@ public class halloween_game{
                   System.out.println("What famous magician died on Halloween?");
                   answer = k.nextLine();
            }
-           System.out.print("\nCorrect!");
+           System.out.print("\nCorrect!\n\n");
          }
         
         
         else if (qu==3){
           System.out.println("What is the most commercially successful horror movie of all time?");
           String rightAnswer= "It";
-          k.nextLine();
+         
            String answer = k.nextLine();
            
 
@@ -83,32 +88,33 @@ public class halloween_game{
                   System.out.println("What is the most commercially successful horror movie of all time");
                   answer = k.nextLine();
            }
-           System.out.print("\nCorrect!");
+           System.out.print("\nCorrect!\n\n");
          }
         
         
         else if (qu==4){
           System.out.println("Where was the first city-wide Halloween celebration in the United States?");
-          String rightAnswer= "Anoka, Minnesota";
-          k.nextLine();
+          String rightAnswer= "Minnesota";
+          String rightAnswer2= "Anoka";
+         
            String answer = k.nextLine();
            
 
-           while (!answer.equalsIgnoreCase(rightAnswer)|| !answer.equalsIgnoreCase("Anoka Minnesota")){
+           while (!answer.equalsIgnoreCase(rightAnswer)&& !answer.equalsIgnoreCase(rightAnswer2)){
 
                   System.out.println("Your answer is wrong. Try again.");
                   System.out.println("\n");
                   System.out.println("Where was the first city-wide Halloween celebration in the United States?");
                   answer = k.nextLine();
            }
-           System.out.print("\nCorrect!");
+           System.out.print("\nCorrect!\n\n");
          }
         
         
         else if (qu==5){
           System.out.println("What candy was given to soldiers in battle in the Korean War?");
           String rightAnswer= "Tootsie Rolls";
-          k.nextLine();
+         
            String answer = k.nextLine();
            
 
@@ -119,14 +125,14 @@ public class halloween_game{
                   System.out.println("What candy was given to soldiers in battle in the Korean War?");
                   answer = k.nextLine();
            }
-           System.out.print("\nCorrect!");
+           System.out.print("\nCorrect!\n\n");
          }
         
         
         else if (qu==6){
           System.out.println("What is the most commercially successful horror franchise of all time?");
           String rightAnswer= "Godzilla";
-          k.nextLine();
+          
            String answer = k.nextLine();
            
 
@@ -137,14 +143,14 @@ public class halloween_game{
                   System.out.println("What is the most commercially successful horror franchise of all time?");
                   answer = k.nextLine();
            }
-           System.out.print("\nCorrect!");
+           System.out.print("\nCorrect!\n\n");
          }
         
         
         else if (qu==7){
           System.out.println("What horror movie was the first American film ever to show a toilet on screen?");
           String rightAnswer= "Psycho";
-          k.nextLine();
+          
            String answer = k.nextLine();
            
 
@@ -155,14 +161,14 @@ public class halloween_game{
                   System.out.println("What horror movie was the first American film ever to show a toilet on screen");
                   answer = k.nextLine();
            }
-           System.out.print("\nCorrect!");
+           System.out.print("\nCorrect!\n\n");
          }
         
         
         else if (qu==8){
           System.out.println("In what American state is it illegal to be a priest or nun for Halloween?");
           String rightAnswer= "Alabama";
-          k.nextLine();
+          
            String answer = k.nextLine();
            
 
@@ -173,14 +179,14 @@ public class halloween_game{
                   System.out.println("In what American state is it illegal to be a priest or nun for Halloween?");
                   answer = k.nextLine();
            }
-           System.out.print("\nCorrect!");
+           System.out.print("\nCorrect!\n\n");
          }
         
         
         else if (qu==9){
           System.out.println("What is the most popular Halloween candy in America?");
           String rightAnswer= "Skittles";
-          k.nextLine();
+          
            String answer = k.nextLine();
            
 
@@ -191,32 +197,32 @@ public class halloween_game{
                   System.out.println("What is the most popular Halloween candy in America?");
                   answer = k.nextLine();
            }
-           System.out.print("\nCorrect!");
+           System.out.print("\nCorrect!\n\n");
          }
         
         
-      //  // else if (qu==10){
-      //  //     System.out.println("What was the original title for Disney's Hocus Pocus?");
-      //  String rightAnswer= "Chicken feed";
-      //      k.nextLine();
-      //       String answer = k.nextLine();
+      else if (qu==10){
+          System.out.println("What was the original title for Disney's Hocus Pocus?");
+        String rightAnswer= "Chicken feed";
+      
+          String answer = k.nextLine();
             
 
-      //       while (!answer.equalsIgnoreCase(rightAnswer)){
+         while (!answer.equalsIgnoreCase(rightAnswer)){
 
-      //              System.out.println("Your answer is wrong. Try again.");
-      //              System.out.println("\n");
-      //              System.out.println("What was candy corn originally called?");
-      //              answer = k.nextLine();
-      //       }
-     // System.out.print("\nCorrect!");
-      //     }
+               System.out.println("Your answer is wrong. Try again.");
+              System.out.println("\n");
+              System.out.println("What was candy corn originally called?");
+              answer = k.nextLine();
+            }
+              System.out.print("\nCorrect!\n\n");
+         }
       
         
         else   {
           System.out.println("What ancient Roman goddess is said to be honored on Halloween?");
           String rightAnswer= "Pomona";
-          k.nextLine();
+          
            String answer = k.nextLine();
            
 
@@ -227,9 +233,10 @@ public class halloween_game{
                   System.out.println("What ancient Roman goddess is said to be honored on Halloween?");
                   answer = k.nextLine();
            }
-           System.out.print("\nCorrect!");
+           System.out.print("\nCorrect!\n\n");
          }
-        
+         count2 ++;
+      }
         
         
         
